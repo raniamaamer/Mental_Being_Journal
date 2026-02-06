6a730fb99da7c8f129cccea3c3c94f133ac9d79b
Rania Maamer <raniamaamer2001@gmail.com>
2026-02-06 16:44:05 +0100

    feat: changer port

--- backend/app.py
+++ backend/app.py
@@ -258,4 +258,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     print(app.url_map)
-    app.run(debug=True)
+    app.run(debug=True, port=4000)
--- next.config.ts
+++ next.config.ts
@@ -5,19 +5,19 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:5000/api/:path*',
+        destination: 'http://127.0.0.1:4000/api/:path*',
       },
       {
         source: '/register',
-        destination: 'http://127.0.0.1:5000/register',
+        destination: 'http://127.0.0.1:4000/register',
       },
       {
         source: '/login',
-        destination: 'http://127.0.0.1:5000/login',
+        destination: 'http://127.0.0.1:4000/login',
       },
       {
         source: '/logout',
-        destination: 'http://127.0.0.1:5000/logout',
+        destination: 'http://127.0.0.1:4000/logout',
       },
     ];
   },
